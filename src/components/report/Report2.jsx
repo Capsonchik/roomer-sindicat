90c7e7b992dc9099938aff2e24d053cb57c43a91
Khaustov.S <Khaustov.S@romir.ru>
2024-06-28 16:31:14 +0300

    Небольшая переработка фильтров во втором варианте

--- src/components/report/Report2.jsx
+++ src/components/report/Report2.jsx
@@ -2,6 +2,7 @@ import {Cascader, CheckPicker, Divider, Panel, Placeholder, Text} from "rsuite";
 import {stack1} from "../../consts/stacks";
 import styles from './styles.module.scss';
 import { mockTreeData } from '../../consts/mock'
+import {useState} from "react";
 
 export const Report2 = () => {
   const data = stack1.map(item => ({ label: item, value: item }));
@@ -14,38 +15,105 @@ export const Report2 = () => {
     }
   });
 
+  const [categoryFilled, setCategoryFilled] = useState(false);
+  const [manufacturerFilled, setManufacturerFilled] = useState(false);
+  const [brandField, setBrandField] = useState(false);
+  const [regionField, setRegionField] = useState(false);
+  const [chanelField, setChanelField] = useState(false);
+  const [netWorkField, setNetWorkField] = useState(false)
+  // Добавьте состояния для остальных CheckPicker'ов
+
+  const handleCategoryChange = (value) => {
+    setCategoryFilled(!!value);
+  };
+
+  const handleManufacturerChange = (value) => {
+    setManufacturerFilled(!!value);
+  };
+
+  const handleBrandChange = (value) => {
+    setBrandField(!!value);
+  }
+
+  const handleRegionChange = (value) => {
+    setRegionField(!!value);
+  }
+
+  const handleChanelChange = (value) => {
+    setChanelField(!!value);
+  }
+
+  const handleNetWorkChange = (value) => {
+    setNetWorkField(!!value);
+  }
+
   return (
     <div className={styles.report2container}>
       <Panel header="Выберете фильтры для необходимой выгрузки*" shaded className={styles.panel}>
         <div className={styles.inputBlocks}>
           <div className={styles.inputContainer}>
             <h6>Категория</h6>
-            <CheckPicker data={data} style={{width: 224}} placeholder={'Категория'}/>
+            <CheckPicker
+              data={data}
+              style={{width: 224}}
+              placeholder={'Категория'}
+              onChange={handleCategoryChange}
+            />
           </div>
 
           <div className={styles.inputContainer}>
             <h6>Производитель</h6>
-            <CheckPicker data={data} style={{width: 224}} placeholder={'Производитель'}/>
+            <CheckPicker
+              data={data}
+              style={{width: 224}}
+              placeholder={'Производитель'}
+              disabled={!categoryFilled}
+              onChange={handleManufacturerChange}
+            />
           </div>
 
           <div className={styles.inputContainer}>
             <h6>Бренд</h6>
-            <CheckPicker data={data} style={{width: 224}} placeholder={'Бренд'}/>
+            <CheckPicker
+              data={data}
+              style={{width: 224}}
+              placeholder={'Бренд'}
+              disabled={!manufacturerFilled}
+              onChange={handleBrandChange}
+            />
           </div>
 
           <div className={styles.inputContainer}>
             <h6>Регион</h6>
-            <CheckPicker data={data} style={{width: 224}} placeholder={'Регион'}/>
+            <CheckPicker
+              data={data}
+              style={{width: 224}}
+              placeholder={'Регион'}
+              disabled={!brandField}
+              onChange={handleRegionChange}
+            />
           </div>
 
           <div className={styles.inputContainer}>
             <h6>Канал</h6>
-            <CheckPicker data={data} style={{width: 224}} placeholder={'Канал'}/>
+            <CheckPicker
+              data={data}
+              style={{width: 224}}
+              placeholder={'Канал'}
+              disabled={!regionField}
+              onChange={handleChanelChange}
+            />
           </div>
 
           <div className={styles.inputContainer}>
             <h6>Сеть</h6>
-            <CheckPicker data={data} style={{width: 224}} placeholder={'Сеть'}/>
+            <CheckPicker
+              data={data}
+              style={{width: 224}}
+              placeholder={'Сеть'}
+              disabled={!chanelField}
+              onChange={handleNetWorkChange}
+            />
           </div>
         </div>
       </Panel>

86b8c751096e3fa48ccff870b6b76419632cf3ae
Khaustov.S <Khaustov.S@romir.ru>
2024-07-01 15:37:12 +0300

    Небольшие правки дизайна

--- src/components/mainComponent/MainComponent.jsx
+++ src/components/mainComponent/MainComponent.jsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss'
-import {Panel, Placeholder} from "rsuite";
+import {Panel} from "rsuite";
 import {News} from "../news/News";
 import {FunnelTime, Scatter, Speaker, Trend} from "@rsuite/icons";
 
@@ -10,19 +10,19 @@ export const MainComponent = () => {
         <div className={styles.topPanel}>
           <div className={styles.panelBlock}>
             <FunnelTime className={styles.icon}/>
-            <h5>20 Отчетов за месяц</h5>
+            <h5 className={styles.blockHeader}>20 Отчетов за месяц</h5>
           </div>
           <div className={styles.panelBlock}>
             <Trend className={styles.icon}/>
-            <h5>10 доступных баз</h5>
+            <h5 className={styles.blockHeader}>10 доступных баз</h5>
           </div>
           <div className={styles.panelBlock}>
             <Scatter className={styles.icon}/>
-            <h5>5 Заявок от клиента</h5>
+            <h5 className={styles.blockHeader}>5 Заявок от клиента</h5>
           </div>
           <div className={styles.panelBlock}>
             <Speaker className={styles.icon}/>
-            <h5>15 Выполненных отчетов</h5>
+            <h5 className={styles.blockHeader}>15 Выполненных отчетов</h5>
           </div>
 
         </div>

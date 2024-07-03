81b22fd7e3d2ad994de68cd01c3c7112398a0ffa
Khaustov.S <Khaustov.S@romir.ru>
2024-07-03 17:09:21 +0300

    Фиксы первого слайда

--- src/components/presintation/slides/slide1/Slide1.jsx
+++ src/components/presintation/slides/slide1/Slide1.jsx
@@ -8,6 +8,10 @@ import PencilSquareIcon from '@rsuite/icons/legacy/PencilSquare';
 import BookIcon from '@rsuite/icons/legacy/Book';
 import WechatIcon from '@rsuite/icons/Wechat';
 import SteamSquareIcon from '@rsuite/icons/legacy/SteamSquare';
+import PeoplesIcon from '@rsuite/icons/Peoples';
+import CalendarIcon from '@rsuite/icons/Calendar';
+import PeoplesCostomizeIcon from '@rsuite/icons/PeoplesCostomize';
+
 
 export const Slide1 = () => {
   const title = 'KPI Категории'
@@ -19,33 +23,67 @@ export const Slide1 = () => {
         <InlineEdit defaultValue={firstSlideMockText}/>
         <div className={styles.slideBlock}>
           <div className={styles.leftBlock} >
-            <Title title={'Total Offtrade'} level={4}/>
-            <Steps current={1} vertical>
-              <Steps.Item
-                status={'finish'}
-                title="Пенетрация 18+"
-                icon={<PencilSquareIcon style={{ fontSize: 36 }} />}
-                description="Описание"
-              />
-              <Steps.Item
-                status={'finish'}
-                title="In Progress"
-                icon={<BookIcon style={{ fontSize: 36 }} />}
-                description="Описание"
-              />
-              <Steps.Item
-                status={'finish'}
-                title="Waiting"
-                icon={<WechatIcon style={{ fontSize: 36 }} />}
-                description="Описание"
-              />
-              <Steps.Item
-                status={'finish'}
-                title="Waiting"
-                icon={<SteamSquareIcon style={{ fontSize: 36 }} />}
-                description="Описание"
-              />
-            </Steps>
+            {/*<Title title={'Total Offtrade'} level={4}/>*/}
+            {/*<Steps current={1} vertical>*/}
+            {/*  <Steps.Item*/}
+            {/*    status={'finish'}*/}
+            {/*    title="Пенетрация 18+"*/}
+            {/*    icon={<PencilSquareIcon style={{ fontSize: 36 }} />}*/}
+            {/*    description="Описание"*/}
+            {/*  />*/}
+            {/*  <Steps.Item*/}
+            {/*    status={'finish'}*/}
+            {/*    title="In Progress"*/}
+            {/*    icon={<BookIcon style={{ fontSize: 36 }} />}*/}
+            {/*    description="Описание"*/}
+            {/*  />*/}
+            {/*  <Steps.Item*/}
+            {/*    status={'finish'}*/}
+            {/*    title="Waiting"*/}
+            {/*    icon={<WechatIcon style={{ fontSize: 36 }} />}*/}
+            {/*    description="Описание"*/}
+            {/*  />*/}
+            {/*  <Steps.Item*/}
+            {/*    status={'finish'}*/}
+            {/*    title="Waiting"*/}
+            {/*    icon={<SteamSquareIcon style={{ fontSize: 36 }} />}*/}
+            {/*    description="Описание"*/}
+            {/*  />*/}
+            {/*</Steps>*/}
+            <div className={styles.totalOffTrade}>
+              <span className={styles.totalOffTradeTitle}>Total Offtrade</span>
+            </div>
+            <div className={styles.totalOffTradeContent}>
+              <div className={styles.totalOffTradeItem}>
+                <div className={styles.totalOffTradeItemLeftBlock}>
+                  <div className={styles.totalOffTradeLogo}><PeoplesIcon style={{fontSize: 26, color: '#FF8200'}}/></div>
+                  <div className={styles.totalOffTradeText}>Пенетрация 18+</div>
+                </div>
+                <div className={styles.totalOffTradeStat}>47.5%</div>
+              </div>
+              <div className={styles.totalOffTradeItem}>
+                <div className={styles.totalOffTradeItemLeftBlock}>
+                  <div className={styles.totalOffTradeLogo}><CalendarIcon style={{fontSize: 26, color: '#FF8200'}}/>
+                  </div>
+                  <div className={styles.totalOffTradeText}>Частота покупки</div>
+                </div>
+                <div className={styles.totalOffTradeStat}>47.5%</div>
+              </div>
+              <div className={styles.totalOffTradeItem}>
+                <div className={styles.totalOffTradeItemLeftBlock}>
+                  <div className={styles.totalOffTradeLogo}><PeoplesCostomizeIcon style={{fontSize: 26, color: '#FF8200'}}/></div>
+                  <div className={styles.totalOffTradeText}>Средний чек</div>
+                </div>
+                <div className={styles.totalOffTradeStat}>47.5%</div>
+              </div>
+              <div className={styles.totalOffTradeItem}>
+                <div className={styles.totalOffTradeItemLeftBlock}>
+                  <div className={styles.totalOffTradeLogo}><PeoplesCostomizeIcon style={{fontSize: 26, color: '#FF8200'}}/></div>
+                  <div className={styles.totalOffTradeText}>Средний объем покупки</div>
+                </div>
+                <div className={styles.totalOffTradeStat}>47.5%</div>
+              </div>
+            </div>
           </div>
           <div className={styles.rightBlock}>
             <Title title={'Вклад KPI в рост объемов'} level={4}></Title>

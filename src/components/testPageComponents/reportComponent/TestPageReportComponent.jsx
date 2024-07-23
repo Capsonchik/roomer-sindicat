43e5b5428404c0022239c201f3b0d41617c477b8
Khaustov.S <Khaustov.S@romir.ru>
2024-07-23 17:14:24 +0300

    Быстрые правки по по странице отчета

--- src/components/testPageComponents/reportComponent/TestPageReportComponent.jsx
+++ src/components/testPageComponents/reportComponent/TestPageReportComponent.jsx
@@ -1,11 +1,25 @@
 import {TestPageFilterComponent} from "../filtersComponents/TestPageFilterComponent";
 import {TestPageGraphComponent} from "../testPageGraphComponent/testPageGraphComponent";
+import {selectGraphs} from "../../../store/reportSlice/reportSlice.selectors";
+import {useSelector} from "react-redux";
+import {Divider} from "rsuite";
 
 export const TestPageReportComponent = () => {
+  const graph = useSelector(selectGraphs)
+
   return (
     <div>
       <TestPageFilterComponent/>
-      <TestPageGraphComponent/>
+      {graph
+        ? <TestPageGraphComponent/>
+        : (
+          <div style={{width: '100%', minHeight: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{width: 500}}>
+              <Divider>Необходимо выбрать группу отчетов</Divider>
+            </div>
+          </div>)
+      }
+
     </div>
   );
 };

0b0448fc8e4e04052570fb4d13b1ff5d781c733b
pavel <pavel_chermyanin@mail.ru>
2024-08-14 12:30:30 +0500

    fix domen

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -22,7 +22,7 @@ export const axiosLoginRequest = createAxiosLoginInstance();
 
 
 // export const CLIENT_API = 'http://192.168.9.239:8808/'
-export const CLIENT_API = 'https://21ce-212-45-6-6.ngrok-free.app';
+export const CLIENT_API = 'https://27ac-212-45-6-6.ngrok-free.app';
 
 const createAxiosClientInstance = () => {
   const instance = axios.create({

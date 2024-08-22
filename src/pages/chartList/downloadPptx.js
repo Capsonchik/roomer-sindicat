48292dd9dd27816cdad51bdc63a49e91e6d35473
pavel <pavel_chermyanin@mail.ru>
2024-08-22 13:40:14 +0300

    add pptx axes visible colors

--- src/pages/chartList/downloadPptx.js
+++ src/pages/chartList/downloadPptx.js
@@ -4,60 +4,81 @@ import {prepareDataForPptx} from "./prepareDataForPptx";
 import {colors} from "./chart/config";
 
 export const downloadPpt = (charts) => {
-    const pptx = new PptxGenJS();
-    const slide = pptx.addSlide();
-    const yOffsetDefault = 2
+  const pptx = new PptxGenJS();
+  const slide = pptx.addSlide();
+  const yOffsetDefault = 2
 
-    const chartWidth = 3; // Ширина графика
-    const chartHeight = 2; // Высота графика
-    const padding = 0.2; // Отступ между графиками
-    let xOffset = 0.2; // Начальная позиция по горизонтали
-    let yOffset = yOffsetDefault; // Начальная позиция по вертикали
+  const chartWidth = 3; // Ширина графика
+  const chartHeight = 2; // Высота графика
+  const padding = 0.2; // Отступ между графиками
+  let xOffset = 0.2; // Начальная позиция по горизонтали
+  let yOffset = yOffsetDefault; // Начальная позиция по вертикали
 
-    charts.forEach((chart, index) => {
-        console.log(chart)
-        const {title, description, xAxisData,seriesData,formatting} = chart;
-        // Добавляем заголовок графика
-        slide.addText(title, {
-            x: xOffset,
-            y: yOffset,
-            align: 'justify',
-            fontSize: 14
-        });
-
-        yOffset += 0.3; // Увеличиваем отступ для описания
-        slide.addText(description, {
-            x: xOffset,
-            y: yOffset,
-            align: 'justify',
-            fontSize: 12
-        });
-        yOffset += 0.3;
-
-        console.log(chart)
-        // Подготовка данных для графика
-        const dataForChart = prepareDataForPptx({xAxisData,seriesData});
-
-        // Определяем направление баров в зависимости от isXAxis
-        const barDirection = formatting.isXAxis ? undefined : 'bar';
-
-        // Увеличиваем отступ для графика
-        // yOffset += 0.5; // Увеличиваем отступ перед графиком
-        slide.addChart('bar', dataForChart, {
-            chartColors:colors.slice(0,dataForChart.length),
-            x: xOffset,
-            y: yOffset,
-            w: chartWidth,
-            h: chartHeight,
-
-            // Используем условное значение для направления баров
-            barDir: barDirection,
-        });
-
-        yOffset = yOffsetDefault; // Увеличиваем отступ после графика
-        xOffset += chartWidth + padding; // Переход к следующей колонке
+  charts.forEach((chart, index) => {
+    console.log(chart)
+    const {title, description, xAxisData, seriesData, formatting} = chart;
+    // Добавляем заголовок графика
+    slide.addText(title, {
+      x: xOffset,
+      y: yOffset,
+      align: 'justify',
+      fontSize: 14
     });
 
-    // Сохранение презентации
-    pptx.writeFile({fileName: 'ChartPresentation.pptx'});
+    yOffset += 0.3; // Увеличиваем отступ для описания
+    slide.addText(description, {
+      x: xOffset,
+      y: yOffset,
+      align: 'justify',
+      fontSize: 12
+    });
+    yOffset += 0.3;
+
+    console.log(chart)
+    // филььруем видимые колонки
+    // const filteredSeriesData = Object.fromEntries(
+    //   Object.entries(seriesData).filter(([name,data] )=> {
+    //   return formatting.visible.includes(name)
+    // })
+    // )
+
+    const filteredSeriesData = Object.fromEntries(Object.entries(seriesData).filter(([name, data]) => {
+      return formatting.visible.includes(name)
+    }))
+
+    // console.log(filteredSeriesData)
+    // Подготовка данных для графика
+    const dataForChart = prepareDataForPptx({
+      xAxisData,
+      seriesData: !!formatting.visible.length ? filteredSeriesData : seriesData
+    });
+
+    // Определяем направление баров в зависимости от isXAxis
+    const barDirection = formatting.isXAxis ? undefined : 'bar';
+
+    // фильтруем цвета
+    const filteredColors = formatting.colors
+      ? formatting.colors.filter(([color, bool]) => bool).map(([color, bool]) => color)
+      : colors.slice(0, dataForChart.length)
+
+
+    // Увеличиваем отступ для графика
+    // yOffset += 0.5; // Увеличиваем отступ перед графиком
+    slide.addChart('bar', dataForChart, {
+      chartColors: filteredColors,
+      x: xOffset,
+      y: yOffset,
+      w: chartWidth,
+      h: chartHeight,
+
+      // Используем условное значение для направления баров
+      barDir: barDirection,
+    });
+
+    yOffset = yOffsetDefault; // Увеличиваем отступ после графика
+    xOffset += chartWidth + padding; // Переход к следующей колонке
+  });
+
+  // Сохранение презентации
+  pptx.writeFile({fileName: 'ChartPresentation.pptx'});
 };

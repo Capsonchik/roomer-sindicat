87d37b8fc03f684ef1e1348b325260cd34c0dca8
capsonchik <Serega00790@mail.ru>
2024-09-24 10:14:02 +0300

    Таблица

---
+++ src/consts/tableMocks.js
@@ -0,0 +1,31 @@
+export const DEFAULT_COLUMNS = [
+    {
+        key: 'id',
+        label: 'Id',
+        fixed: true,
+        width: 70
+    },
+    {
+        key: 'firstName',
+        label: 'First Name',
+        fixed: true,
+        width: 130
+    },
+    {
+        key: 'lastName',
+        label: 'Last Name',
+        width: 123
+    },
+
+    {
+        key: 'gender',
+        label: 'Gender',
+        width: 200
+    },
+    {
+        key: 'city',
+        label: 'City',
+        flexGrow: 1
+    }
+];
+
--- src/pages/chartList/chartItemTable/ChartItemTable.jsx
+++ src/pages/chartList/chartItemTable/ChartItemTable.jsx
@@ -1,12 +1,31 @@
 import styles from './chartItemTable.module.scss'
 import {useDispatch} from "react-redux";
-import {Button} from "rsuite";
+import {Button, HStack, Table, TagPicker, Toggle, VStack} from 'rsuite';
 import {setActiveChart, setOpenDrawer} from "../../../store/chartSlice/chart.slice";
 import EditIcon from "@rsuite/icons/Edit";
-import React from "react";
+import React, {useState} from "react";
+import {DEFAULT_COLUMNS} from "../../../consts/tableMocks";
+import {data} from "../../../consts/tableData";
+
+
+const {Column, HeaderCell, Cell} = Table;
+const CompactCell = props => <Cell {...props} style={{padding: 4}}/>;
+const CompactHeaderCell = props => <HeaderCell {...props} style={{padding: 4}}/>;
+
 
 export const ChartItemTable = ({chart}) => {
   const dispatch = useDispatch();
+
+  const [compact, setCompact] = useState(true);
+  const [bordered, setBordered] = useState(true);
+  const [showHeader, setShowHeader] = useState(true);
+  const [hover, setHover] = useState(true);
+  const [columnKeys, setColumnKeys] = useState(DEFAULT_COLUMNS.map(column => column.key));
+
+  const columns = DEFAULT_COLUMNS.filter(column => columnKeys.some(key => key === column.key));
+  const CustomCell = compact ? CompactCell : Cell;
+  const CustomHeaderCell = compact ? CompactHeaderCell : HeaderCell;
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.title_wrapper}>
@@ -20,8 +39,55 @@ export const ChartItemTable = ({chart}) => {
         </Button>
       </div>
       ChartItemTable
-      {/*<p>{chart.description}</p>*/}
-      {/*<div ref={chartRef} style={{width: '100%', minHeight: '400px'}}></div>*/}
+      <VStack spacing={16}>
+        <HStack>
+          <Toggle checked={compact} onChange={setCompact}>
+            Compact
+          </Toggle>
+
+          <Toggle checked={bordered} onChange={setBordered}>
+            Bordered
+          </Toggle>
+
+          <Toggle checked={showHeader} onChange={setShowHeader}>
+            Show Header
+          </Toggle>
+
+          <Toggle checked={hover} onChange={setHover}>
+            Hover
+          </Toggle>
+        </HStack>
+        <TagPicker
+          data={DEFAULT_COLUMNS}
+          labelKey="label"
+          valueKey="key"
+          value={columnKeys}
+          onChange={setColumnKeys}
+          cleanable={false}
+        />
+      </VStack>
+      <hr/>
+
+      <Table
+        height={300}
+        hover={hover}
+        showHeader={showHeader}
+        data={data}
+        bordered={bordered}
+        cellBordered={bordered}
+        headerHeight={compact ? 30 : 40}
+        rowHeight={compact ? 30 : 46}
+      >
+        {columns.map(column => {
+          const {key, label, ...rest} = column;
+          return (
+            <Column {...rest} key={key}>
+              <CustomHeaderCell>{label}</CustomHeaderCell>
+              <CustomCell dataKey={key}/>
+            </Column>
+          );
+        })}
+      </Table>
 
     </div>
   )

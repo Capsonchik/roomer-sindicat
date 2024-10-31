d53fb0e9537d7f7d1edc092d43d1663a249a31e0
pavel <pavel_chermyanin@mail.ru>
2024-10-31 12:11:54 +0300

    add graph

--- src/pages/chartList/chartItemTree/CyptoTree.jsx
+++ src/pages/chartList/chartItemTree/CyptoTree.jsx
@@ -1,36 +1,56 @@
 import React, {useEffect, useRef, useState} from "react";
 import cytoscape from "cytoscape";
 import dagre from "cytoscape-dagre";
-import {CheckPicker} from "rsuite";
+import {Button, CheckPicker} from "rsuite";
 import "rsuite/dist/rsuite.min.css"; // Подключаем стили для CheckPicker
 import nodeHtmlLabel from "cytoscape-node-html-label";
 import styles from './cyptoTree.module.scss'
+import cl from "classnames";
+import {pieMocks} from "../chartEditor/chartPie/pie-mocks";
+import {setActiveChart, setOpenDrawer} from "../../../store/chartSlice/chart.slice";
+import EditIcon from "@rsuite/icons/Edit";
 
 // Подключаем плагины
 cytoscape.use(dagre);
 nodeHtmlLabel(cytoscape); // Подключаем nodeHtmlLabel плагин
 
 const initialState = [
-  {data: {id: "p", label: "Узел P",type: 'one', values: [{value: 2444, percent: '+20'},{value: 2444, percent: '+20'}, {value: 999, percent: '+20'}]}},
-  {data: {id: "r", label: "Узел R",type: 'one', values: [{value: 2444, percent: '+20'}, {value: 999, percent: '+20'}]}},
-  {data: {id: "a", label: "Узел A",type: 'one', values: [{value: 1333, percent: '+20'}, {value: 1663, percent: '+20'}]}},
-  {data: {id: "q", label: "Узел Q",type: 'two', values: [{value: 2444, percent: '+20'}, {value: 999, percent: '+20'}]}},
-  {data: {id: "b", label: "Узел B",type: 'one', values: [{value: 4545, percent: '+20'}, {value: 7878, percent: '-20'}]}},
-  {data: {id: "c", label: "Узел C",type: 'two', values: [{value: 2323, percent: '+20'}, {value: 656, percent: '+20'}]}},
-  {data: {id: "d", label: "Узел D",type: 'two', values: [{value: 454, percent: '-20'}, {value: 787, percent: '+20'}]}},
-  {data: {id: "f", label: "Узел F",type: 'two', values: [{value: 2121, percent: '+20'}, {value: 6767, percent: '-20'}]}},
-  {data: {id: "g", label: "Узел G",type: 'one', values: [{value: 232, percent: '-20'}, {value: 6767, percent: '+20'}]}},
-  {data: {id: "h", label: "Узел GH  ",type: 'one', values: [{value: 232, percent: '-20'}, {value: 6767, percent: '+20'}]}},
-  {data: {source: "a", target: "b"}},
-  {data: {source: "a", target: "p"}},
-  {data: {source: "p", target: "r"}},
-  {data: {source: "p", target: "q"}},
-  {data: {source: "b", target: "d"}},
-  {data: {source: "b", target: "f"}},
-  {data: {source: "b", target: "c"}},
-  {data: {source: "c", target: "g"}},
-  {data: {source: "c", target: "p"}},
-  {data: {source: "f", target: "g"}},
+  {data: {id: "1", label: "Объем, тыс.p",type: 'one', values: [{value: 460409750, percent: '+41971767'},{value: 502381517, percent: '+9,1%'}]},position: { x: 100, y: 100 }},
+  {data: {id: "2", label: "Объем, тыс.л.",type: 'one', values: [{value: 3933628, percent: '+79325'}, {value: 4012953, percent: '+2,0%'}]},position: { x: -300, y: 250 }},
+  {data: {id: "3", label: "Цена за литр",type: 'two', values: [{value: 117, percent: '+8,1р'}, {value: 125, percent: '+7,0%'}]},position: { x: -550, y: 350 }},
+
+  {data: {id: "4", label: "Объем, тыс.шт",type: 'one', values: [{value: 6358247, percent: '+343916'}, {value: 6702163, percent: '+5,4%'}]},position: { x: 550, y: 250 }},
+  {data: {id: "5", label: "Цена за шт",type: 'two', values: [{value: 72, percent: '+2,5р'}, {value: 75, percent: '+3,5%'}]},position: { x: 700, y: 350 }},
+
+  {data: {id: "6", label: "Трафик",type: 'two', values: [{value: 2702844441, percent: '+122447150'}, {value: 2825291591, percent: '+4,5%'}]},position: { x: -100, y: 400 }},
+  {data: {id: "7", label: "Средний чек",type: 'one', values: [{value: 170, percent: '+7,5р'},{value: 178, percent: '+4,4%'}]},position: { x: 200, y: 350 }},
+
+
+  {data: {id: "8", label: "Объем покупки,л",type: 'two', values: [{value: 1.46, percent: '-0,03'},{value: 1.42, percent: '-2,4%'}]},position: { x: -400, y: 650 }},
+  {data: {id: "9", label: "Покупатели",type: 'two', values: [{value: 59619489, percent: '+3116953'}, {value: 62736442, percent: '+5,2%'}]},position: { x: -100, y: 520 }},
+  {data: {id: "10", label: "Популяция",type: 'two', values: [{value: 104581474, percent: '+1'}, {value: 104581474, percent: '+0,0%'}]},position: { x: -220, y: 650 }},
+  {data: {id: "11", label: "Пенетрация",type: 'one', values: [{value: '57,0%', percent: '+3,0п.п'}, {value: '60,0%', percent: '+5,2%'}]},position: { x: 0, y: 650 }},
+  {data: {id: "12", label: "Пенетрация 18+",type: 'one', values: [{value: '27,0%', percent: '+3,0п.п'}, {value: '30,0%', percent: '+5,2%'}]},position: { x: 200, y: 650 }},
+  {data: {id: "13", label: "Частота покупки",type: 'one', values: [{value: 45.3, percent: '-0,3'}, {value: '45.0', percent: '-0,7'}]},position: { x: 400, y: 650 }},
+  {data: {id: "14", label: "Объем покупки,шт",type: 'two', values: [{value: 2.4, percent: '+0,02'}, {value: 3.4, percent: '+0,8'}]},position: { x: 600, y: 650 }},
+
+
+
+  {data: {source: "1", target: "2"}},
+  {data: {source: "1", target: "3"}},
+  {data: {source: "1", target: "4"}},
+  {data: {source: "1", target: "5"}},
+  {data: {source: "1", target: "6"}},
+  {data: {source: "1", target: "7"}},
+  {data: {source: "2", target: "8"}},
+  {data: {source: "6", target: "8"}},
+  {data: {source: "6", target: "9"}},
+  {data: {source: "9", target: "10"}},
+  {data: {source: "9", target: "11"}},
+
+  {data: {source: "6", target: "13"}},
+  {data: {source: "4", target: "14"}},
+  {data: {source: "6", target: "14"}},
 ];
 
 export const CytoscapeTree = () => {
@@ -44,11 +64,9 @@ export const CytoscapeTree = () => {
       container: cyRef.current,
       elements: initialState,
       layout: {
-        name: "dagre",
-        rankDir: "TB",
-        nodeSep: 150,
-        edgeSep: 10,
-        rankSep: 100,
+
+        // Привязываем граф к верхнему левому краю
+        name: "preset" // Используем preset для позиционирования по координатам из position
       },
       style: [
         {
@@ -83,6 +101,10 @@ export const CytoscapeTree = () => {
       wheelSensitivity: 0.1,  // Чувствительность зума (меньшее значение = более плавный зум
     });
 
+    cy.viewport({
+      zoom: 0.7,
+      pan: { x: 700, y: 0 }
+    });
     setCyInstance(cy);
 
     // Настройка сложного содержимого узлов
@@ -95,16 +117,16 @@ export const CytoscapeTree = () => {
         halignBox: 'center',
         tpl: (data) => {
           return (
-            `<div style="color: red; border-radius: 5px; width: 150px; height: 60px; border: 1px solid #ccc; z-index: 1000; background: white">
+            `<div style="color: red; border-radius: 5px; min-width: 150px; height: 65px; border: 1px solid #ccc; z-index: 1000; background: white">
       <div style="background: ${data.type === 'one' ? '#88b6e8' :'#c2cfdf'}; color: #fff; text-align: center">
         <strong>${data.label}</strong>
       </div>
-      <div style="display: flex; height: 40px">
+      <div style="display: flex; height: 43px">
         ${data.values.map((item, index) => {
               return (
                 `<div style="flex-grow: 1; text-align: center; ${index < data.values.length - 1 ? 'border-right: 1px solid #ccc;' : ''}">
-              <div style="color:#050c26">${item.value}</div>
-              <div  style="color: ${item.percent.startsWith('+') ? 'green': 'red'};border-top: 1px solid #ccc">${item.percent}</div>
+              <div style="color:#050c26; padding-inline: 5px">${item.value}</div>
+              <div  style="color: ${item.percent.startsWith('+') ? 'green': 'red'};border-top: 1px solid #ccc;padding-inline: 5px">${item.percent}</div>
             </div>`
               );
             }).join('')}
@@ -138,6 +160,7 @@ export const CytoscapeTree = () => {
       cyInstance.layout({
         name: "dagre",
         rankDir: "TB",
+        align: "UL",
         nodeSep: 150,
         edgeSep: 10,
         rankSep: 100,
@@ -161,30 +184,32 @@ export const CytoscapeTree = () => {
 
   return (
     <>
-      {/*<button onClick={() => {*/}
-      {/*  setEdges(initialState.filter(el => el.data.source));*/}
-      {/*  if (cyInstance) {*/}
-      {/*    cyInstance.elements().remove();*/}
-      {/*    cyInstance.add(initialState);*/}
-      {/*    cyInstance.layout({*/}
-      {/*      name: "dagre",*/}
-      {/*      rankDir: "TB",*/}
-      {/*      nodeSep: 150,*/}
-      {/*      edgeSep: 10,*/}
-      {/*      rankSep: 100,*/}
-      {/*      animate: true,*/}
-      {/*    }).run();*/}
-      {/*  }*/}
-      {/*}}>Сброс*/}
-      {/*</button>*/}
-      <div >
-      {/*<div style={{display: 'flex', marginTop: "10px", height: '100%'}}>*/}
+
+
+      <div className={cl(styles.wrapper, {
+
+      })}>
+        <div className={styles.title_wrapper}>
+          <h5>Деревья KPI</h5>
+          {/*<Button className={styles.btn} onClick={() => {*/}
+
+          {/*  // dispatch(setActiveChart(chart))*/}
+          {/*  // dispatch(setOpenDrawer(true))*/}
+          {/*}}>*/}
+          {/*  <EditIcon/>*/}
+          {/*</Button>*/}
+        </div>
         <div
           className={styles.wrapper}
           id="cy"
           ref={cyRef}
-          style={{width: "100%",margin:'0 auto', height: "500px", border: "1px solid #ccc"}}
+          style={{width: "100%", margin: '0 auto', height: "500px", border: "1px solid #ccc"}}
         />
+
+      </div>
+      {/*<div>*/}
+        {/*<div style={{display: 'flex', marginTop: "10px", height: '100%'}}>*/}
+
         {/*<div style={{marginLeft: "20px", display: 'flex', flexDirection: 'column'}}>*/}
         {/*  <h3>Управление узлами и связями</h3>*/}
         {/*  <div >*/}
@@ -205,7 +230,7 @@ export const CytoscapeTree = () => {
         {/*  </div>*/}
 
         {/*</div>*/}
-      </div>
+      {/*</div>*/}
     </>
   );
 };
